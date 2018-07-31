2ada453753c6d9c546fda7706eda129a2c1ef02c
Branden Mosley <branden.mosley1337@gmail.com>
2018-07-31 19:31:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,6 +19,9 @@ function calculatesFarePrice(start, end) {
   let distance = distanceTravelledInFeet(start, end);
   if (distance < 400) {
     return 0;
+  } else if (distance > 400 && distance < 2000) {
+    result = (distance - 400) * 0.02
+    return result
   }
   
 }
