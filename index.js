9463ff0e4913d26fe2f8bfbfc1c32187bb8ec8c5
Branden Mosley <branden.mosley1337@gmail.com>
2018-07-31 19:24:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,3 +3,9 @@ function distanceFromHqInBlocks(street) {
   result = Math.abs(street - 42);
   return result
 }
+
+function distanceFromHqInFeet(street) {
+  distance = distanceFromHqInBlocks(street);
+  result = distance * 264
+  return result
+}
